6d78e2ac9dfd7352565a55b3c883532d62f70753
alexrivadeneira <alexrivs@gmail.com>
2018-04-01 18:55:29 -0700

    ok

--- src/main/java/com/example/usersapi/UsersApiApplication.java
+++ src/main/java/com/example/usersapi/UsersApiApplication.java
@@ -2,9 +2,15 @@ package com.example.usersapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class UsersApiApplication {
+
+	@RequestMapping("/")
+	public String home(){
+		return "some users";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UsersApiApplication.class, args);
--- src/test/java/com/example/usersapi/UsersApiApplicationTests.java
+++ src/test/java/com/example/usersapi/UsersApiApplicationTests.java
@@ -1,16 +1,65 @@
 package com.example.usersapi;
 
+import com.example.usersapi.models.User;
+import com.example.usersapi.repositories.UserRepository;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.stream.Stream;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.core.Is.is;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class UsersApiApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Autowired
+	private UserRepository userRepository;
+
+	@Before
+	public void setUp(){
+		userRepository.deleteAll();
 	}
+
+
+	@After
+	public void tearDown(){
+		userRepository.deleteAll();
+	}
+
+	@Test
+	public void shouldAllowFullCrudForAUser() throws Exception{
+		User firstUser = new User(
+				"someone",
+				"Ima",
+				"Person"
+		);
+
+		User secondUser = new User(
+			"someone_else",
+			"Someone",
+			"Else"
+		);
+
+		Stream.of(firstUser, secondUser)
+				.forEach(user -> {
+					userRepository.save(user);
+				});
+
+		when()
+				.get("http://localhost:8080/users")
+				.then()
+				.statusCode(is(200))
+				.and().body(containsString("someone"))
+				.and().body(containsString("Else"));
+	}
+
 
 }
