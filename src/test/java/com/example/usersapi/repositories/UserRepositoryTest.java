f5c43af9d2da1b7bf01363764c3fbbf705186c27
alexrivadeneira <alexrivs@gmail.com>
2018-04-02 22:55:53 -0700

    change buildgradle

---
+++ src/test/java/com/example/usersapi/repositories/UserRepositoryTest.java
@@ -0,0 +1,58 @@
+import com.example.usersapi.models.User;
+import com.example.usersapi.repositories.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.internal.util.collections.Iterables;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class UserRepositoryTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp() {
+        User firstUser = new User(
+                "user_name",
+                "some first name",
+                "some last name"
+        );
+
+        User secondUser = new User(
+                "second_user",
+                "some other first name",
+                "some other last name"
+        );
+
+        entityManager.persist(firstUser);
+        entityManager.persist(secondUser);
+        entityManager.flush();
+    }
+
+    @Test
+    public void findAll_returnsAllUsers() {
+        Iterable<User> usersFromDb = userRepository.findAll();
+
+        int resultSize;
+
+        resultSize = (int) usersFromDb.spliterator().getExactSizeIfKnown();
+
+        assertThat(resultSize, is(2));
+    }
+
+
+}
