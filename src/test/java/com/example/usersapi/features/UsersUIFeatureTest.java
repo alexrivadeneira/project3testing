dd5df75295ee53bfb60676d02b2476b058f909ad
alexrivadeneira <alexrivs@gmail.com>
2018-04-03 10:14:47 -0700

    users ui feature

--- src/test/java/com/example/usersapi/features/UsersUIFeatureTest.java
+++ src/test/java/com/example/usersapi/features/UsersUIFeatureTest.java
@@ -1,4 +1,54 @@
 package com.example.usersapi.features;
 
+import com.example.usersapi.models.User;
+import com.example.usersapi.repositories.UserRepository;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.stream.Stream;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class UsersUIFeatureTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Before
+    public void setUp(){
+        userRepository.deleteAll();
+    }
+
+    @After
+    public void tearDown(){
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void shouldAllowFullCrudFunctionalityForAUser() throws Exception {
+        System.setProperty("selenide.browser", "Chrome");
+
+        User firstUser = new User(
+                "someone",
+                "Ima",
+                "Person"
+        );
+
+        User secondUser = new User(
+                "someone_else",
+                "Someone",
+                "Else"
+        );
+
+        Stream.of(firstUser, secondUser)
+                .forEach(user -> {
+                    userRepository.save(user);
+                });
+
+    }
 }
